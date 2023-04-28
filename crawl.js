607d1e722806111b6ec1d54ab58f2e2c031bb3ec
thenodenovice <thenodenovice@gmail.com>
2023-04-28 13:31:11 +0100

    write tests

--- crawl.js
+++ crawl.js
@@ -1,3 +1,43 @@
+const { urlencoded } = require('body-parser')
+const {JSDOM} = require('jsdom')
+
+const getURLsFromHTML = (htmlBody, baseURL)=> {
+    const urls = []
+    
+    const dom = new JSDOM(htmlBody)
+   
+    const links = dom.window.document.querySelectorAll('a')
+    
+    links.forEach((link)=>{
+        
+       if(link.href.slice(0,1) === '/'){
+           //relative url
+           try{
+            const urlObj = new URL(`${baseURL}${link.href}`)
+            return urls.push(urlObj.href)
+
+           }catch(e){
+               console.log(e.message)
+           }
+           
+       }else{
+        //absolute url
+        try{
+            const urlObj = new URL(link.href)
+            urls.push(urlObj.href)
+
+        }catch(e){
+            console.log(e.message)
+        }
+          
+       }
+       
+           
+    })
+
+    return urls
+}
+
 const normalizeURL = (urlString) => {
 
     const url = new URL(urlString)
@@ -13,5 +53,6 @@ const normalizeURL = (urlString) => {
 
 
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsFromHTML
 }
--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,4 @@
-const {normalizeURL} = require('./crawl')
+const {normalizeURL, getURLsFromHTML} = require('./crawl')
 const {test,expect} = require('@jest/globals')
 
 test('normalizeURL strip protocol',()=>{
@@ -26,6 +26,76 @@ test('normalizeURL remove capitalization',()=>{
     const input = 'https://ADEBOLA.com/thenodenovice'
     const actual = normalizeURL(input)
     const expected = 'adebola.com/thenodenovice'
+
+    expect(actual).toEqual(expected)
+
+})
+
+test('getURLsFromHTML absolute path',()=>{
+
+    const inputHTML = `
+    <html>
+        <body>
+            <a href="https://adebola.com/thenodenovice"> Adebola</a>
+        </body>
+    </html>
+    `
+    const inputURL = 'https://adebola.com/thenodenovice'
+    const actual = getURLsFromHTML(inputHTML, inputURL)
+    const expected = ['https://adebola.com/thenodenovice']
+
+    expect(actual).toEqual(expected)
+
+})
+
+test('getURLsFromHTML relative path',()=>{
+
+    const inputHTML = `
+    <html>
+        <body>
+            <a href="/thenodenovice"> Adebola</a>
+        </body>
+    </html>
+    `
+    const inputURL = 'https://adebola.com'
+    const actual = getURLsFromHTML(inputHTML, inputURL)
+    const expected = ['https://adebola.com/thenodenovice']
+
+    expect(actual).toEqual(expected)
+
+})
+
+test('getURLsFromHTML multiple path',()=>{
+
+    const inputHTML = `
+    <html>
+        <body>
+            <a href="/thenodenovice"> Adebola</a>
+            <a href="/theShadu"> Adebola</a>
+        </body>
+    </html>
+    `
+    const inputURL = 'https://adebola.com'
+    const actual = getURLsFromHTML(inputHTML, inputURL)
+    const expected = ['https://adebola.com/thenodenovice','https://adebola.com/theShadu']
+
+    expect(actual).toEqual(expected)
+
+})
+
+test('getURLsFromHTML bad url',()=>{
+
+    const inputHTML = `
+    <html>
+        <body>
+            <a href="invalid">invalid</a>
+
+        </body>
+    </html>
+    `
+    const inputURL = 'https://adebola.com'
+    const actual = getURLsFromHTML(inputHTML, inputURL)
+    const expected = []
 
     expect(actual).toEqual(expected)
 
